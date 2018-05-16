5c485d3435a6e2d03c2f4005da03a164dce2e9b4
Randy Reitz <rsreitz@outlook.com>
2018-05-16 13:34:31 -0400

    Added drink api endpoints.  Added TODO items for the rest of the endpoints.

--- controllers/_example.js
+++ controllers/_example.js
@@ -12,17 +12,4 @@ module.exports = {
     //     // This returns a promise to the external code, so it can access our results
     //     return $functionName;
     // }
-
-    findAll: function(user) {
-
-        // findAll returns all entries for a table when used with no options
-        var $findall = db.User.findAll({}).then(function(dbUser) {
-            // return our results when the function is called
-            return dbUser;
-        });
-        // This returns a promise to the external code, so it can access our results
-        return $findall;
-    }
-
-
 }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,141 +5,61 @@
 // Requiring our model controllers
 // TODO: Figure out how we can use an index.js like sequelize does for models...
 // var control = require("../controllers");
+var drink = require("../controllers/drink");
 var user = require("../controllers/user");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-    //#region User Functions
-    // POST route for saving a new user
-    app.post("/api/signup", function(req, res) {
-        // console.log(req.body[0]);
-        // findAll returns all entries for a table when used with no options
-        user.addUser(req.body[0]).then(function(dbuser) {
-            // We have access to the user as an argument inside of the callback function
-            res.json(dbuser);
-        });
-    });
+    //#region Category Functions
+    //TODO: // get all categories
 
-    // POST route for saving a new user
-    app.post("/api/login", function(req, res) {
-        // console.log(req.body[0]);
-        // login returns user info if they submit the credentials.
-        user.login(req.body[0].userName, req.body[0].passKey, ).then(function(dbuser) {
-            // console.log(dbuser)
-            // We have access to the user as an argument inside of the callback function
-            res.json(dbuser);
-        });
-    });
-
-    // GET route for retrieving all users
-    app.get("/api/user", function(req, res) {
-        // console.log(req.body[0]);
-        // findAll returns all entries for a table when used with no options
-        user.findAll({}).then(function(dbuser) {
-            // console.log(dbuser)
-            // We have access to the user as an argument inside of the callback function
-            res.json(dbuser);
-        });
-    });
-
-    // GET route for retrieving user by username
-    app.get("/api/user/:username", function(req, res) {
-        // findAll returns all entries for a table when used with no options
-        user.findUserName(req.params.username).then(function(dbuser) {
-            // console.log(dbuser)
-            // We have access to the user as an argument inside of the callback function
-            res.json(dbuser);
-        });
-    });
-
-    //TODO: Convert to Controller
-    app.put("/api/username", function(req, res) {
-        db.update(req.body, {
-                where: {
-                    username: req.body.username
-                }
-            })
-            .then(function() {
-                res.json();
-            });
-    });
-    //#endregion User Functions
+    //#endregion Category Functions  
 
     //#region Drink Functions
 
-    //TODO: Convert to Controller
     //  find drink
     app.get("/api/drink/:drink", function(req, res) {
-        db.findAll({
-                where: {
-                    drink: req.params.drink
-                }
-            })
-            .then(function() {
-                res.json();
+        drink.findDrink(req.params.drink)
+            .then(function(dbResults) {
+                // We have access to the results as an argument inside of the callback function
+                res.json(dbResults);
             });
     });
-    //TODO: Convert to Controller
+
     // add drink
     app.post("/api/drink", function(req, res) {
-        console.log(req.body);
-        db.create({
-                drink: req.body.drink,
-                ingredients: req.body.ingredients,
-                location: req.body.location,
-            })
-            .then(function() {
-                res.json();
+        console.log(req.body[0]);
+        // findAll returns all entries for a table when used with no options
+        drink.addDrink(req.body[0])
+            .then(function(dbResults) {
+                // We have access to the results as an argument inside of the callback function
+                res.json(dbResults);
             });
     });
-    //TODO: Convert to Controller
-    // update drink
-    app.put("/api/drink", function(req, res) {
-        db.update(req.body, {
-                where: {
-                    drink: req.body.drink
-                }
-            })
-            .then(function() {
-                res.json();
-            });
-    });
-    //#endregion Drink Functions
-
-    //#region Ingredient Functions
-    //TODO: Convert to Controller
-    app.get("/api/ingredients/:ingredients", function(req, res) {
-        db.findAll({
-                where: {
-                    ingredients: req.params.ingredients
-                }
-            })
-            .then(function() {
-                res.json();
-            });
-    });
-    //#endregion Ingredient Functions
-
-    //#region Review Functions
-    //#endregion Review Functions    
-
-    //#region Friend Functions
-
-    //TODO: Review and mabye convert to Controller
-    // app.get("/api/friendname/:friendname", function(req, res) {
-    //     db.findAll({
+    // //TODO: Convert to Controller
+    // // update drink
+    // app.put("/api/drink", function(req, res) {
+    //     db.update(req.body, {
     //             where: {
-    //                 friendname: req.params.friendname
+    //                 drink: req.body.drink
     //             }
     //         })
     //         .then(function() {
     //             res.json();
     //         });
     // });
+    //#endregion Drink Functions
+
+    //#region Friend Functions
+    //TODO: // Find all Friends by user
+    //TODO: // Remove Friend
+    //TODO: // Find all pending friend requests
+    //TODO: // Accept Friend
 
     //TODO: Convert to Controller
+    // Add Friend
     app.post("/api/friends", function(req, res) {
         console.log(req.body);
         db.create({
@@ -152,14 +72,48 @@ module.exports = function(app) {
     });
     //#endregion Friend Functions    
 
-    //#region Vote Functions
-    //#endregion Vote Functions    
+    //#region Ingredient Functions
+    //TODO: // Get all ingredients
+    //TODO: // Add ingredient
+    //TODO: // Retire ingredient
 
-    //#region Review Functions
-    //#endregion Review Functions    
-
-    //#region Review Functions
     //TODO: Convert to Controller
+    // Find ingredient by name
+    app.get("/api/ingredients/:ingredients", function(req, res) {
+        db.findAll({
+                where: {
+                    ingredients: req.params.ingredients
+                }
+            })
+            .then(function() {
+                res.json();
+            });
+    });
+    //#endregion Ingredient Functions
+
+    //#region Part Functions
+    //TODO: // Add drink part (ingredient)
+    //TODO: // update drink part
+
+    //#endregion Part Functions  
+
+    //#region Place Functions
+    //TODO: // Add place
+    //TODO: // Update place
+    //TODO: // Mark Closed
+
+    //#endregion Place Functions  
+
+    //#region Review Functions
+
+    //TODO: //Get Reviews by user
+
+    //TODO: //Get Reviews by drink
+
+    //TODO: //Get Reviews by location
+
+    //TODO: Convert to Controller
+    //Add Review
     app.post("/api/review", function(req, res) {
         console.log(req.body);
         db.create({
@@ -172,6 +126,7 @@ module.exports = function(app) {
     });
 
     //TODO: Convert to Controller
+    //Edit Review
     app.put("/api/review", function(req, res) {
         db.update(req.body, {
                 where: {
@@ -183,5 +138,67 @@ module.exports = function(app) {
             });
     });
     //#endregion Review Functions    
+
+    //#region User Functions
+    // POST route for saving a new user
+    app.post("/api/signup", function(req, res) {
+        // console.log(req.body[0]);
+        // findAll returns all entries for a table when used with no options
+        user.addUser(req.body[0]).then(function(dbResults) {
+            // We have access to the user as an argument inside of the callback function
+            res.json(dbResults);
+        });
+    });
+
+    // POST route for saving a new user
+    app.post("/api/login", function(req, res) {
+        // console.log(req.body[0]);
+        // login returns user info if they submit the credentials.
+        user.login(req.body[0].userName, req.body[0].passKey, ).then(function(dbResults) {
+            // console.log(dbResults)
+            // We have access to the user as an argument inside of the callback function
+            res.json(dbResults);
+        });
+    });
+
+    // GET route for retrieving all users
+    app.get("/api/user", function(req, res) {
+        // console.log(req.body[0]);
+        // findAll returns all entries for a table when used with no options
+        user.findAll({}).then(function(dbResults) {
+            // console.log(dbResults)
+            // We have access to the user as an argument inside of the callback function
+            res.json(dbResults);
+        });
+    });
+
+    // GET route for retrieving user by username
+    app.get("/api/user/:username", function(req, res) {
+        // findAll returns all entries for a table when used with no options
+        user.findUserName(req.params.username).then(function(dbResults) {
+            // console.log(dbResults)
+            // We have access to the user as an argument inside of the callback function
+            res.json(dbResults);
+        });
+    });
+
+    // //TODO: Convert to Controller
+    // app.put("/api/username", function(req, res) {
+    //     db.update(req.body, {
+    //             where: {
+    //                 username: req.body.username
+    //             }
+    //         })
+    //         .then(function() {
+    //             res.json();
+    //         });
+    // });
+    //#endregion User Functions
+
+    //#region Vote Functions
+    //TODO: // Add Upvote
+    //TODO: // Add Downvote
+    //TODO: // Remove vote
+    //#endregion Vote Functions    
 
 };
