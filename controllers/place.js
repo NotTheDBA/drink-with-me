acbfc385d5fa3d88f3cb579e1192f18bcbedb1be
Randy Reitz <rsreitz@outlook.com>
2018-05-16 14:30:06 -0400

    Added stubs for controllers.

--- controllers/place.js
+++ controllers/place.js
@@ -2,14 +2,57 @@ var db = require("../models");
 
 module.exports = {
     //TODO:  Write functions
-    //   //template
-    // functionName: function(parm) {
-    //     // functionName does something
-    //     var $functionName = db.User.findAll({}).then(function(results) {
-    //         // return our results from the function
-    //         return results;
-    //     });
-    //     // This returns a promise to the external code, so it can access our results
-    //     return $functionName;
-    // }
+    findAll: function() {
+
+        // findAll returns all entries for a table when used with no options
+        var $findall = db.model.findAll({}).then(function(dbResults) {
+            // return our results from the function
+            return dbResults;
+        });
+        // This returns a promise to the external code, so it can access our results
+        return $findall;
+    },
+
+    findOneByProperty: function(property) {
+
+        // findByProperty does something
+        var $findByProperty = db.Model.findOne({
+            where: {
+                propertyName: property
+            }
+        }).then(function(results) {
+            // return our results from the function
+            return results;
+        });
+        // This returns a promise to the external code, so it can access our results
+        return $findByProperty;
+    },
+
+    addModel: function(property1, property2, property3) {
+
+        // create takes an argument of an object for our model
+        var $addModel =
+            db.Model.create({
+                property1: model.property1,
+                property2: model.property2,
+                property3: model.property3,
+                // placeID: placeId
+                placeId: 0,
+                createdBy: model.createdBy,
+                updatedBy: model.updatedBy
+                    // ingredients: req.body.ingredients,
+                    // location: req.body.location,
+            }).then(function(dbResults) {
+                // We have access to the new todo as an argument inside of the callback function
+                return dbResults;
+            })
+            .catch(function(err) {
+                // Whenever a validation or flag fails, an error is thrown
+                // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+                return err;
+            });
+
+        // This returns a promise to the external code, so it can access our results
+        return $addModel;
+    }
 }
