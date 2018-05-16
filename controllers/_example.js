8aa00f9efd5154483582fdecce6c66ff83580e34
Randy Reitz <rsreitz@outlook.com>
2018-05-16 11:39:29 -0400

    User logins

--- controllers/_example.js
+++ controllers/_example.js
@@ -2,6 +2,17 @@ var db = require("../models");
 
 module.exports = {
 
+    //   //template
+    // functionName: function(parm) {
+    //     // functionName does something
+    //     var $functionName = db.User.findAll({}).then(function(results) {
+    //         // return our results from the function
+    //         return results;
+    //     });
+    //     // This returns a promise to the external code, so it can access our results
+    //     return $functionName;
+    // }
+
     findAll: function(user) {
 
         // findAll returns all entries for a table when used with no options
