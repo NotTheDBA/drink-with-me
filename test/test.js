55acc425a2a3477431f9d9320a03027e5de91955
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-19 12:20:09 -0400

    updated test.js and html

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var app = require('../routes/api-routes.js');
+var app = require('../server.js');
 var should = chai.should();
 var expect = chai.expect;
 
@@ -24,7 +24,7 @@ var login_details = {
 
 //Log in page
 describe("API Routes", function () {
-
+    //6
     describe("POST /api/signup", function () {
         it("should take the input from the email, password, and age certification and send to the database", function () {
             chai.request(app)
@@ -32,12 +32,12 @@ describe("API Routes", function () {
                 .send(register_details)
                 .end(function (err, res) {
                     res.should.have.status(201);
-                    
+
                 })
         })
     });
 
-
+    //7
     describe("POST /api/login", function () {
         it("should login", function () {
             chai.request(app)
@@ -45,82 +45,131 @@ describe("API Routes", function () {
                 .send(login_details)
                 .end(function (err, res) {
                     res.should.have.status(200);
-                  
+
                 })
         })
     });
-
-    describe("GET /api/username:username", function () {
+    //4
+    describe("GET /api/username/:username", function () {
         it("should get the username and information", function () {
             chai.request(app)
                 .get("/api/username:username")
-                .end(function (err, res) {
+                .then((res) => {
                     res.should.have.status(200);
-                    expect(res.body.state).to.be.true;
-                    res.body.data.should.be.an('object');
-                    done()
+                    res.should.be.json;
+                })
+        });
+    });
+    //1
+    describe("GET /api/all", function () {
+        it("should get the list of drinks the user has rated", function () {
+            chai.request(app)
+                .get("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+                .then((res) => {
+                    res.should.have.status(200);
+
+                })
+        });
+    });
+    //2
+    describe("GET /api/drink:drink", function () {
+        it("should get name, location, star rating, and text rating, and ingredient list of drink ", function () {
+            chai.request(app)
+                .get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+                .then((res) => {
+                    res.should.have.status(200);
+
+                })
+        });
+    });
+    //10
+    describe("GET /api/friend", function () {
+        it("should find a friend for the user to follow", function () {
+            chai.request(app)
+                .get("/api/friend")
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
                 })
         });
     });
 
-    // describe("GET /api/username/drinks", function () {
-    //     it("should upon page load get the list of drinks the user has rated", function () {
-    //         // expect
-    //     });
-    // });
-
-    // describe("GET /api/username/drink", function () {
-    //     it("should upon click get the name, location, star rating, and text rating, and ingredient list of drink ", function () {
-    //         // expect
-    //     });
-    // });
-
-    // describe("POST api/friends", function () {
-    //     it("should upon input and click find a friend for the user to follow", function () {
-    //         // expect
-    //     });
-    // });
-
 
     // //Add a drink page
-
-    // describe("POST api/drink", function () {
-    //     it("should take the input from the drink name, location, star rating, and text rating and send it to the db", function () {
-    //         // expect
-    //     });
-    // });
-
-    // describe(" GET /api/drink/ingredients", function () {
-    //     it("should get a list of ingredients from the db to add to our drink", function () {
-    //         // expect
-    //     });
-    // });
+    //8
+    describe("POST api/drink", function () {
+        it("should take the input from the drink name, location, star rating, and text rating and send it to the db", function () {
+            chai.request(app)
+                .post("/api/drink")
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                })
+        });
+    });
+    //3
+    describe(" GET /api/ingredient/:ingredient", function () {
+        it("should get a list of ingredients from the db to add to our drink", function () {
+            chai.request(app)
+                .get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+                .then((res) => {
+                    res.should.have.status(200);
+                })
+        });
+    });
 
     // //Update a drink page
-
-    // describe("PUT /api/drink", function () {
-    //     it("should upon click update a certain part of the review", function () {
-    //         // expect
-    //     });
-    // });
+    //12
+    describe("PUT /api/drink", function () {
+        it("should upon click update a drink", function () {
+            chai.request(app)
+                .put("/api/drink")
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                })
+        });
+    });
 
 
     // //Following list page
-    // describe("GET /api/friend", function () {
-    //     it("should get a list of friends the user is following from the db and display on the page", function () {
-    //         // expect
-    //     });
-    // });
+    //5
+    describe("GET /api/username/:friend", function () {
+        it("should get a list of friends the user is following from the db and display on the page", function () {
+            chai.request(app)
+                .get("/api/username/friend")
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                })
+        });
+    });
 
 
     // //Add a review to a drink page
+    //11
+    describe("PUT /api/review", function () {
+        it("should add an additional text review to a drink", function () {
+            chai.request(app)
+                .put("/api/review")
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                })
+        });
+    });
+    //Post a review of a drink
 
-    // describe("POST /api/review", function () {
-    //     it("should upon click add an additional text review to a drink", function () {
-    //         // expect
-    //     });
-    // });
-
-    // })
+    //9 
+    describe("POST /api/review", function () {
+        it("should add a text review to a drink", function () {
+            chai.request(app)
+                .post("/api/review")
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                })
+        });
+    });
 
 });
