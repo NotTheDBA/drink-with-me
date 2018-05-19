0b94d709402bbd20d180a4e51769be4bd06f741d
Randy Reitz <rsreitz@outlook.com>
2018-05-19 12:03:27 -0400

    Added cleanup for seeders so that we don't keep adding to the table every restart...

--- seeders/20180519151836-coctail_seeds.js
+++ seeders/20180519151836-coctail_seeds.js
@@ -83,5 +83,6 @@ module.exports = {
           Example:
           return queryInterface.bulkDelete('Person', null, {});
         */
+        return queryInterface.bulkDelete('Categories', null, {});
     }
 };
