abacfcb975d276b9bb028118da883d5787675c43
4RSYS\rreitz <rreitz@4rsystems.com>
2018-05-29 16:46:17 -0400

    more routing work

--- migrations/20180519153933-create-category.js
+++ migrations/20180519153933-create-category.js
@@ -1,33 +1,35 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Categories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      general: {
-        type: Sequelize.STRING
-      },
-      specific: {
-        type: Sequelize.STRING
-      },
-      cocktailDbId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Categories');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Categories', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            general: {
+                type: Sequelize.STRING
+            },
+            specific: {
+                type: Sequelize.STRING
+            },
+            cocktailDbId: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.NOW
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.NOW
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Categories');
+    }
 };
